e44916e5a9531a848d331c0f8e329b9dba5ecb3a
Joseph Ding <josephding@Josephs-Laptop.local>
2024-11-24 18:23:42 -0500

    add github link for projects

--- src/components/ProjectDetailsModal.js
+++ src/components/ProjectDetailsModal.js
@@ -12,6 +12,7 @@ class ProjectDetailsModal extends Component {
       var title = this.props.data.title;
       var description = this.props.data.description;
       var url = this.props.data.url;
+      var github_url = this.props.data.github_url;
       if (this.props.data.technologies) {
         var tech = technologies.map((icons, i) => {
           return (
@@ -89,6 +90,19 @@ class ProjectDetailsModal extends Component {
                   <i
                     className="fas fa-external-link-alt"
                     style={{ marginLeft: "10px" }}
+                  ></i>
+                </a>
+              ) : null}
+              {github_url ? (
+                <a
+                  href={github_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link-href"
+                >
+                  <i
+                    className="fab fa-github"
+                    style={{ marginLeft: "10px", color: "white" }}
                   ></i>
                 </a>
               ) : null}
